c38fe16c4ef10180e96307b0922f6b2e18f7b3eb
clerici03 <115361946+clerici03@users.noreply.github.com>
2022-12-11 16:31:04 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
 
+#include "lib.h"
+
+float numeromax ( float numeri[], int dim) {
+    numeri [dim];
+    float massimo= numeri[0];
+    for (int i = 1; i < dim; i++) {
+        if (massimo < numeri[i]) {
+            massimo = numeri[dim];
+        }
+    }
+
+}
