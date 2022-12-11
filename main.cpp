f9b8c284cb0e8a73eccf760e8a7d2d3a026764a1
clerici03 <115361946+clerici03@users.noreply.github.com>
2022-12-11 16:28:31 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
-
+using namespace std;
 int main(){
+  int dim=0;
+    cin >> dim;
+    float numeri [dim];
+    for (int i=0; i< dim; i++){
+        cin >> numeri[i];
+    }
+    numeromax (numeri, dim);
+    cout<< numeromax (numeri,dim) <<endl;
   return 0;
 }
