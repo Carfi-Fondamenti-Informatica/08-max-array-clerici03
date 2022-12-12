7961968417ef3508a6f8e645f426467ee325add8
clerici03 <115361946+clerici03@users.noreply.github.com>
2022-12-12 22:35:20 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -3,8 +3,8 @@
 float numeromax (float b[], int n) {
     float c=1;
     for (int j=0; j<n; j++) {
-        if (numeri[j]>c) {
-            c =numeri[j];
+        if (b[j]>c) {
+            c =b[j];
         }
     }
     return c;
